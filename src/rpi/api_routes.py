22d193c5540ef25451bbbab3714ef4f973842e85
Hilson Jesus <72456717+Jesushilson@users.noreply.github.com>
2026-02-21 18:18:43 -0500

    Adds a Get/status call

--- src/rpi/api_routes.py
+++ src/rpi/api_routes.py
@@ -3,10 +3,25 @@ from datetime import datetime, timezone
 
 
 def register_routes(app):
+    # Check the health of the RPI server
     @app.get("/health")
     def health():
         return jsonify({
             "ok": True,
             "ts": datetime.now(timezone.utc).isoformat(),
             "service": "pill-dispenser-api"
+        })
+    
+    state = {
+        "status": "idle",
+        "job": {
+            "container": 0,
+            "queue": 0
+        }
+    }
+    # Check the status of the system
+    @app.get("/status")
+    def status():
+        return jsonify({
+            state
         })
