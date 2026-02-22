24404d8db13aa29e6773c209644dd4c6d3b22a00
Hilson Jesus <72456717+Jesushilson@users.noreply.github.com>
2026-02-22 04:07:48 -0500

    More functionality in motor control
    Motors have a set path that matches the hardware

--- src/rpi/motor_control.py
+++ src/rpi/motor_control.py
@@ -28,17 +28,33 @@ step_sequence = [
     [0,0,0,1]
 ]
 
-# Step Counter for both motors 
-motor1_step_counter = 0
-motor2_step_counter = 0
+# Universal Distance Between Containers
+STEPS_BETWEEN = 2400
+
+# what the program thinks the 1st mechanism is at
+current_container = 1 
+
+# what the program thinks the 2nd mechanism is at
+current_size = 1
+
+size_dict = {
+    1 : "large",
+    2 : "medium",
+    3: "small", 
+    4: "long",
+    5: "short"
+}
+
+
+
 
 # Steps the motor 
-def step_motor(steps, motorType, motor_step_counter, direction=True):
+def step_motor(steps, motorType, direction=True):
     if motorType == "motor_1":
         motor_pins = [m1_in1, m1_in2, m1_in3, m1_in4]
     else:
         motor_pins = [m2_in1, m2_in2, m2_in3, m2_in4]
-
+    motor_step_counter = 0
     
     GPIO.setmode(GPIO.BCM)
 
@@ -66,11 +82,37 @@ def step_motor(steps, motorType, motor_step_counter, direction=True):
             GPIO.output(pin, GPIO.LOW)
         GPIO.cleanup()
 
-def moveToContainer():
-    step_motor(800, "motor_1", motor1_step_counter)
+def moveToContainer(target_container):
+    global current_container
 
-def moveToSize(size):
-    step_motor(800, "motor_2", motor2_step_counter, False)
+    diff = current_container - target_container
+    if diff == 0:
+        return
+    steps = abs(diff) * STEPS_BETWEEN
+     
+    # Go the direction where the desination is the closest
+    direction = True if diff > 0 else False 
+
+    step_motor(steps, "motor 1", direction)
+    current_container = target_container
+
+def moveToSize(target_size):
+    global current_size
+
+    diff = current_size - target_size
+    if diff == 0:
+        return
+    steps = abs(diff) * STEPS_BETWEEN
+
+    direction = True if diff > 0 else False 
+
+    step_motor(steps, "motor 2", direction)
+    current_size = target_size
+
+def restart_mechanisms():
+    moveToContainer(1)
+    moveToSize(1)
+
 
 if __name__ == "__main__":
     moveToContainer()
